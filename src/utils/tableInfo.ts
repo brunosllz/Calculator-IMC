4f680e1bee3d92b0536ae0467bb887dcf98ee258
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-09-21 21:23:20 -0300

    Ajust tab info max and min value

--- src/utils/tableInfo.ts
+++ src/utils/tableInfo.ts
@@ -3,31 +3,31 @@ export const tableInfo = [
     name: 'Abaixo do peso',
     description: 'Abaixo de 18,5',
     minValue: 1,
-    maxValue: 18.4
+    maxValue: 18.49
   },
   {
     name: 'Peso normal',
     description: 'Entre 18,5 e 24,9',
     minValue: 18.5,
-    maxValue: 24.9
+    maxValue: 24.99
   },
   {
     name: 'Sobrepeso',
     description: 'Entre 25 e 29,9',
     minValue: 25,
-    maxValue: 29.9
+    maxValue: 29.99
   },
   {
     name: 'Obesidade Grau I',
     description: 'Entre 30 e 34,9',
     minValue: 30,
-    maxValue: 34.9
+    maxValue: 34.99
   },
   {
     name: 'Obesidade Grau II',
     description: 'Entre 35 e 39,9',
     minValue: 35,
-    maxValue: 39.9
+    maxValue: 39.99
   },
   {
     name: 'Obesidade Grau III',
